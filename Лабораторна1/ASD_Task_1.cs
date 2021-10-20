7b56031fb267351eb2346a10aff70a2249e63b17
Sofia <sofia.pisotska@gmail.com>
2021-10-20 19:51:51 +0300

    Лабораторна готова!!!

---
+++ Лабораторна1/ASD_Task_1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            double x = rnd.NextDouble() * 100 - 50;
+            double y = rnd.NextDouble() * 100 - 50;
+            double z = rnd.NextDouble() * 100 - 50;
+            double a = 0, b = 0, n = 0, l = 0;
+            double m = x * x + x * y * y + x * x * z;
+            if (m > 0)
+            {
+                a = 1 + Sin(x) / Sqrt(m);
+            }
+            if (a != 0)
+            {
+                n = y + z / a;
+            }
+            if (n != 0)
+            {
+                l = x + a / n;
+            }
+            if (l != 0)
+            {
+                b = Log(a * a * a + x * x) / l;
+            }
+            else
+            {
+                WriteLine("Error");
+            }
+            WriteLine("a=" + a);
+            WriteLine("b=" + b);
+        }
+    }
+}
---
+++ Лабораторна1/ASD_Task_2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, a = 0, b = 0, c = 0, d = 0;
+                for (int i = 10; i < 100; i++)
+                {
+                    num = i;
+                    a = num / 10;
+                    b = num % 10;
+                    if (a * a + b * b == num)
+                    {
+                        WriteLine(num);
+                    }
+                }
+                for (int i = 100; i < 1000; i++)
+                {
+
+                    num = i;
+                    a = num / 100;
+                    b = (num % 100) / 10;
+                    c = num - a * 100 - b * 10;
+                    if (Pow(a, 3) + Pow(b, 3) + Pow(c, 3) == num)
+                    {
+                        WriteLine(num);
+                    }
+                }
+                for (int i = 1000; i < 10000; i++)
+                {
+                    num = i;
+                    a = num / 1000;
+                    b = (num - a * 1000) / 100;
+                    c = (num - a * 1000 - b * 100) / 10;
+                    d = num - a * 1000 - b * 100 - c * 10;
+                    if (Pow(a, 4) + Pow(b, 4) + Pow(c, 4) + Pow(d, 4) == num)
+                    {
+                        WriteLine(num);
+                    }
+                }
+            
+            
+        }
+    }
+}
+
